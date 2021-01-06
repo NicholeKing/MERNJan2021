4a23f51157965baccb28b6544484f6ca7de3d6ad
NicholeKing <nichole.king94@hotmail.com>
2021-01-06 17:58:13 -0500

    React Demo

---
+++ first-project/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <p>Hello world!</p>
+      <input htmlFor="something"/>
+    </div>
+    <div>hello</div>
+    <h3>Hello world</h3>
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+      <li>Four</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
